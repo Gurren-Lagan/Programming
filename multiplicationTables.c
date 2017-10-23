79c994c7fbd7435d9130f82fe1c2aece3f4c1dc7
Yair_GL <saint_seiya_1844@hotmail.com>
2017-10-23 03:09:10 -0500

    XD

--- multiplicationTables.c
+++ multiplicationTables.c
@@ -1,17 +1,15 @@
+//multiplying tables from 1 to 10 of whole numbers y GL
 #include <stdio.h>
 
 int main(){
-  int n=9,i;
+  int n,i;
 
-//scanf("%d",&n);
+scanf("%d",&n);
 
   for(i=1;i<=10;i++){
 
     printf("%2d x %3d = %4d\n",i,n,(i*n));
   }
-
-
-
-
+  
   return 0;
 }
