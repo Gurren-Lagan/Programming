174063430330d6627084f77c06fa4ed829260f08
Yair_GL <saint_seiya_1844@hotmail.com>
2017-10-23 02:46:56 -0500

    Add files via upload
    math

---
+++ powermath.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+  int n,p,i,powr=1;
+
+  printf("Enter the number: ");
+  scanf("%d",&n);
+  printf("Enter the power: ");
+  scanf("%d",&p);
+
+  for(i=0;i<p;i++){
+    powr*=n;
+  }
+
+  printf("%d\n",powr);
+}
+
+return 0;
+}
