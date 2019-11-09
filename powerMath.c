3e9a3953342eb306306cc178590b96132222f2f6
YairReyes18 <azaelyair@hotmail.com>
2019-11-09 00:10:56 -0600

    update

--- powerMath.c
+++ powerMath.c
@@ -2,20 +2,15 @@
 #include <stdio.h>
 
 int main(){
-
   int n,p,i,powr=1;
-
   printf("Enter the number: ");
   scanf("%d",&n);
   printf("Enter the power: ");
   scanf("%d",&p);
-
   for(i=0;i<p;i++){
     powr*=n;
   }
-
   printf("%d\n",powr);
-}
-
-return 0;
+  
+  return 0;
 }
